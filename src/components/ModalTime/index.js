01eb7e5a1d84c6571bbe03e88018f21c14f931fa
Vinícius Melo Oliveira <viniciusmelo241@gmail.com>
2021-01-18 21:35:24 -0300

    Estrutura do ModalTime montada

--- src/components/ModalTime/index.js
+++ src/components/ModalTime/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
 
+import { Head, LeftButton, Title, RightButton, Main, SubText, Description, Foot, Time } from './style';
+
+import clock from '../../assets/clock.svg';
+import confirm from '../../assets/confirm.svg';
+
 export default function ModalTime() {
 
     return(
-        <h1>Eu</h1>
+        <>
+            <Head>
+                <LeftButton src={confirm} alt="confirm" />
+                <Title> Interval! </Title>
+                <RightButton> X </RightButton>
+            </Head>
+            <Main>
+                <SubText> Take some glass of water </SubText>
+                <Description> Math: Odd and Even numbers. </Description>
+            </Main>
+            <Foot>
+                <Time><span id="clock">03:37</span>&nbsp;<img src={clock} alt="clock" /></Time>
+            </Foot>
+        </>
     )
 }
--- src/components/ModalTime/style.js
+++ src/components/ModalTime/style.js
@@ -1,3 +1,37 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
-import styled from 'styled-component';
+export const Head = styled.div`
+
+`;
+
+export const LeftButton = styled.img`
+
+`;
+
+export const Title = styled.h1`
+
+`;
+
+export const RightButton = styled.h1`
+
+`;
+
+export const Main = styled.div`
+
+`;
+
+export const SubText = styled.p`
+
+`;
+
+export const Description = styled.h3`
+
+`;
+
+export const Foot = styled.div`
+
+`;
+
+export const Time = styled.div`
+
+`;
